ab5f21b1cec7c666d8b14b5984eccddaf2cda723
Anjel <angelica.gomez@unwritten.studio>
2026-05-11 21:15:56 +0200

    fix: add About component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
@@ -9,6 +10,7 @@ export default function Home() {
       <Header />
       <main>
         <Hero />
+        <About />
         <Projects />
       </main>
       <Footer />
